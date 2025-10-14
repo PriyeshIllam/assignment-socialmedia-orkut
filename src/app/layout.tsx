cd5fec3830d67fd305ceac556943726a66ba0ad4
PriyeshIllam <seeujob@gmail.com>
2025-10-14 06:36:42 +0200

    home page after login

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,7 +1,6 @@
 // src/app/layout.tsx
 import './globals.scss'
 import { ReactNode } from 'react'
-import AuthProvider from './AuthProvider'
 import ClientLayout from './ClientLayout'
 
 export const metadata = {
@@ -13,9 +12,7 @@ export default function RootLayout({ children }: { children: ReactNode }) {
   return (
     <html lang="en">
       <body>
-        <AuthProvider>
-          <ClientLayout>{children}</ClientLayout>
-        </AuthProvider>
+        <ClientLayout>{children}</ClientLayout>
       </body>
     </html>
   )
